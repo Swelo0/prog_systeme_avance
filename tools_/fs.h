86287620aa94c1b350516836972908d311979dca
DomKiki <DomKiki@hotmail.com>
2014-11-26 11:46:18 +0100

    Update fs.h

--- tools_/fs.h
+++ tools_/fs.h
@@ -1,8 +1,30 @@
 #ifndef H_FS_
 #define H_FS_
 
-void sfscreate();
-void sfsadd();
+typedef short int16;
+
+typedef char block[1024];
+
+// attribute(packed) pour empêcher le compilateur de faire du panning
+typedef struct filesystem { // __attribute__(__packed__) {
+
+	struct superblock {
+		char    signature[8]	 ; 
+		int16 	sectors_per_block;
+		int16 	bitmap_size	 ;
+		int16 	file_entry_size  ;
+		int16 	file_entry_blocks;
+		int16 	file_content_size;
+	} sb          		;
+
+	block bitmap            ;
+	block file_entry[16]    ;
+	block file_content[8191];
+		
+} filesystem;
+
+filesystem sfscreate();
+void sfsadd(char file[]);
 void sfslist();
 void sfsdel();
 
