4f95c2ce7f4236fe0e3a95340764fba44bac7a96
Gurrek <dow_sachari@hotmail.fr>
2014-11-26 09:54:24 +0100

    Système de fichiers
    squellette, je dois le finir

---
+++ tools_/fs.c
@@ -0,0 +1,60 @@
+// fs.c
+
+typedef short 	   int16;
+typedef char[1024] block;
+
+typedef struct superblock {
+	char[8] signature		 ; 
+	int16 	sectors_per_block;
+	int16 	bitmap_size		 ;
+	int16 	file_entry_size  ;
+	int16 	file_entry_blocks;
+	int16 	file_content_size;
+} superblock;
+
+typedef struct filesystem {
+	//superblock   sb          ;
+	struct superblock {
+		char[8] signature		 ; 
+		int16 	sectors_per_block;
+		int16 	bitmap_size		 ;
+		int16 	file_entry_size  ;
+		int16 	file_entry_blocks;
+		int16 	file_content_size;
+	} 			 sb          ;
+	block	     bitmap      ;
+	block[16]    file_entry  ;
+	block[8191]  file_content;		
+} filesystem;
+
+filesystem sfscreate() {
+		filesystem sf;
+		
+		sf.sb.signature 		 = "SFSv0100";
+		sf.sb.sectors_per_block  = 2         ;
+		sf.sb.bitmap_size        = 1         ;
+		sf.sb.file_entry_size    = 256       ;
+		sf.sb.file_entry_blocks  = 16        ;
+		sf.sb.file_content_size  = 8192      ;
+		
+		//Init du bitmap à 0;
+		sf.bitmap = 0;
+		//Init du file entries à 0;
+		int i;
+		for (i=0; i<16; i++)
+			sf.file_entry[i] = 0;
+		
+		return sf;
+}
+
+void sfsadd() {
+	// Code was here
+}
+
+void sfslist() {
+	// Code was here
+}
+
+void sfsdel() {
+	// Code was here
+}
---
+++ tools_/fs.h
@@ -0,0 +1,9 @@
+#ifndef H_FS_
+#define H_FS_
+
+void sfscreate();
+void sfsadd();
+void sfslist();
+void sfsdel();
+
+#endif
