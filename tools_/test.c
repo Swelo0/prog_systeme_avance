f0ecd36e17c5c2343dc1602e3867c9ece10e91c2
Gurrek <sacha.teles@gmail.com>
2014-12-03 14:52:17 +0100

    changement init var

--- tools_/test.c
+++ tools_/test.c
@@ -4,6 +4,12 @@
 int main() {
 	filesystem fs = sfscreate();
 	sfsadd(fs, "test.txt");
+	///////////////////
+	numEntry = 0;
+	while (fs.fe[numEntry].name[0] == '\0')
+		numEntry++;
+	
+	printf("fichier : %s -> %d \n",fs.fe[numEntry].name, numEntry);
 	return 0;
 
 }
